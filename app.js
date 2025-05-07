422159220de7b8f6a11b47ec7e03ffd60761e244
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-07 12:42:36 +0300

    java3

---
+++ app.js
@@ -0,0 +1,15 @@
+// types of variables
+// var name = "ahmed"
+// let name = "ahmed"
+// const name = "ahmed"
+
+let firstName = "ahmed"
+console.log(firstName)
+
+let secondName = "yussuf"
+console.log(secondName)
+
+let middleName = "ibrahim"
+console.log(middleName)
+
+console.log(firstName + " " + secondName + " " + middleName)
