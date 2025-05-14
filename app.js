c02f1bfee08a9aa01b97c72f4a424b87c92397be
Ahmedu22 <ahmedgithub2236@gmail.com>
2025-05-14 11:35:08 +0300

    java3

--- app.js
+++ app.js
@@ -13,3 +13,37 @@ let middleName = "ibrahim"
 console.log(middleName)
 
 console.log(firstName + " " + secondName + " " + middleName)
+
+// java fourth-lesson
+
+// js Numbers
+let firstFavNum = 12;
+let sencondFavNum = 23;
+console.log(firstFavNum + sencondFavNum);
+console.log(firstFavNum - sencondFavNum);
+console.log(firstFavNum * sencondFavNum);
+console.log(firstFavNum / sencondFavNum);
+console.log(firstFavNum % sencondFavNum);
+
+
+// relational operators
+
+// >, <, =
+
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+
+// equality operators
+console.log(10 === 10);
+console.log(10 !== 10);
+
+// control follow
+
+let v = 7;
+if(v==7){
+console.log('yes')
+}
+else{
+    console.log("no")
+}
